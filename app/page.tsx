86457f023e49c0072179b1ae827b7bf74ae4d5ca
vraj <vjariwala745@gmail.com>
2025-03-06 07:48:10 -0800

    ADDED NEW ARRIVAL SECTION ON HOME PAGE

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ import ReviewSection from '@/components/shared/home/ReviewSection'
 import SpecialCombos from '@/components/shared/home/SpecialCombos'
 import { fetchAllWebsiteBanners } from '@/lib/database/actions/banners.actions'
 import { getAllCrazyDealOffers, getAllSpecialComboOffers } from '@/lib/database/actions/homescreenoffers.actions'
-import { getTopSellingProducts } from '@/lib/database/actions/product.actions'
+import { getNewArrivalProducts, getTopSellingProducts } from '@/lib/database/actions/product.actions'
 import { getAllSubCategoriesByName } from '@/lib/database/actions/subCategory.actions'
 
 import React from 'react'
@@ -31,7 +31,12 @@ const Homepage = async  ()  => {
     console.log(err)
   );
   
-    const transformedBestSellerProducts = topSellingProducts?.products.map(
+
+  const newArrivalProducts = await getNewArrivalProducts().catch((err) =>
+    console.log(err)
+  );
+
+  const transformedBestSellerProducts = topSellingProducts?.products.map(
     (product: any) => ({
       id: product._id,
       name: product.name,
@@ -55,6 +60,29 @@ const Homepage = async  ()  => {
     })
   );
   
+  const transformedNewArrivalProducts = newArrivalProducts?.products.map(
+    (product: any) => ({
+      id: product._id,
+      name: product.name,
+      category: product.category, // You might need to format this
+      image: product.subProducts[0]?.images[0].url || "", // Adjust to match your image structure
+      rating: product.rating,
+      reviews: product.numReviews,
+      price: product.subProducts[0]?.price || 0, // Adjust to match your pricing structure
+      originalPrice: product.subProducts[0]?.originalPrice || 0, // Add logic for original price
+      discount: product.subProducts[0]?.discount || 0,
+      isBestseller: product.featured,
+      isSale: product.subProducts[0]?.isSale || false, // Adjust if you have sale logic
+      slug: product.slug,
+      prices: product.subProducts[0]?.sizes
+        .map((s: any) => {
+          return s.price;
+        })
+        .sort((a: any, b: any) => {
+          return a - b;
+        }),
+    })
+  );
   return (
     <div>
       <BannerCarousel desktopImages={desktopImages}/>
@@ -63,6 +91,7 @@ const Homepage = async  ()  => {
       <CategorySection subCategories={subcategoriesData.subCategories}/>
       <CrazyDeals dealsData={crazyDealsData}/>
       <NeedOfWebsite/>
+      <ProductCard heading="NEW ARRIVALS" products={transformedNewArrivalProducts} />
       <ReviewSection/>
       <BlogImages/>
       </div>
